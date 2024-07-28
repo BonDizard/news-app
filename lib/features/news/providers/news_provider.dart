5d54c602e559f1307845558b5125abda448ac3f7
BonD <8123bharathbond8123@gmail.com>
2024-07-28 22:09:54 +0530

    test 0.0.6 Remote config

--- lib/features/news/providers/news_provider.dart
+++ lib/features/news/providers/news_provider.dart
@@ -1,32 +1,59 @@
 import 'dart:async';
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:pingo_learn_news_app/features/news/controller/news_view_model.dart';
 import '../../../models/news_channel_headline_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class NewsProvider with ChangeNotifier {
   NewsChannelHeadlinesModel? _newsChannelHeadlines;
   String _selectedCountry = 'in'; // Default country is 'in'
+  bool _isCached = false;
 
   final _newsStreamController =
       StreamController<NewsChannelHeadlinesModel?>.broadcast();
 
   String get selectedCountry => _selectedCountry;
-
   NewsChannelHeadlinesModel? get newsChannelHeadlines => _newsChannelHeadlines;
+  bool get isCached => _isCached;
 
   Stream<NewsChannelHeadlinesModel?> get newsStream =>
       _newsStreamController.stream;
+
   final _newsViewModel = NewsViewModel();
 
   void setSelectedCountry(String country, BuildContext context) {
     _selectedCountry = country;
-    _newsViewModel.fetchNewsChannelHeadlineApi(context: context);
+    fetchNews(context);
+  }
+
+  Future<void> fetchNews(BuildContext context) async {
+    final prefs = await SharedPreferences.getInstance();
+    final cachedNews = prefs.getString('news_$_selectedCountry');
+
+    if (cachedNews != null) {
+      _newsChannelHeadlines =
+          NewsChannelHeadlinesModel.fromJson(jsonDecode(cachedNews));
+      _newsStreamController.sink.add(_newsChannelHeadlines);
+    } else {
+      _newsViewModel.fetchNewsChannelHeadlineApi(context: context);
+    }
+
     notifyListeners();
   }
 
-  void setNewsChannelHeadlines(NewsChannelHeadlinesModel newsChannelHeadlines) {
+  void setNewsChannelHeadlines(NewsChannelHeadlinesModel newsChannelHeadlines,
+      {bool isCached = false}) async {
     _newsChannelHeadlines = newsChannelHeadlines;
+    _isCached = isCached;
     _newsStreamController.sink.add(_newsChannelHeadlines);
+
+    if (!isCached) {
+      final prefs = await SharedPreferences.getInstance();
+      prefs.setString(
+          'news_$_selectedCountry', jsonEncode(newsChannelHeadlines.toJson()));
+    }
+
     notifyListeners();
   }
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:pingo_learn_news_app/core/common/error_text.dart';
-import 'package:pingo_learn_news_app/core/common/loader.dart';
+import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'core/utils/routes.dart';
 import 'package:routemaster/routemaster.dart';
 import 'package:provider/provider.dart';
@@ -17,50 +16,12 @@ import 'models/user_model.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MyApp());
-}
-
-class MyApp extends StatefulWidget {
-  MyApp({Key? key}) : super(key: key);
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  UserModel? userModel;
-
-  @override
-  void initState() {
-    super.initState();
-    // Move getData call to initState to avoid multiple calls
-    WidgetsBinding.instance?.addPostFrameCallback((_) {
-      if (Provider.of<User?>(context, listen: false) != null) {
-        getData(context, Provider.of<User?>(context, listen: false)!);
-      }
-    });
-  }
-
-  Future<void> getData(BuildContext context, User data) async {
-    print('getData called');
-    try {
-      userModel = await Provider.of<AuthController>(context, listen: false)
-          .getUserData(data.uid)
-          .first;
-      final userProvider = Provider.of<UserProvider>(context, listen: false);
-      userProvider.updateUser(userModel);
-      setState(() {});
-      print('getData completed');
-    } catch (error) {
-      print('Error in getData: $error');
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final height = MediaQuery.of(context).size.height * 1;
-
-    return MultiProvider(
+  final remoteConfig = FirebaseRemoteConfig.instance;
+  await remoteConfig.setDefaults(const {
+    'countryCode': 'in', // Set your default values here
+  });
+  runApp(
+    MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => NewsProvider()),
         ChangeNotifierProvider(create: (_) => UserProvider()),
@@ -79,56 +40,120 @@ class _MyAppState extends State<MyApp> {
           initialData: null,
         ),
       ],
-      child: Consumer<User?>(
-        builder: (context, data, child) {
-          return MaterialApp.router(
-            theme: ThemeData(
-              textTheme: TextTheme(
-                displayLarge: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'Poppins',
-                  fontSize: height * 0.02,
-                  color: Colors.black,
-                ),
-                displayMedium: TextStyle(
-                  fontWeight: FontWeight.normal,
-                  fontFamily: 'Poppins',
-                  fontSize: height * 0.015,
-                  color: Colors.black,
-                ),
-                bodyLarge: TextStyle(
-                  fontFamily: 'Poppins',
-                  fontSize: height * 0.01,
-                  color: Colors.black,
-                ),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  UserModel? userModel;
+  String countryCode = 'in';
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      if (Provider.of<User?>(context, listen: false) != null) {
+        await getData(context, Provider.of<User?>(context, listen: false)!);
+      }
+      final remoteConfig = FirebaseRemoteConfig.instance;
+      countryCode = remoteConfig.getString('countryCode');
+      remoteConfig.onConfigUpdated.listen((RemoteConfigUpdate event) async {
+        await remoteConfig.activate();
+        setState(() {
+          countryCode = remoteConfig.getString('countryCode');
+        });
+        Provider.of<NewsProvider>(context, listen: false)
+            .setSelectedCountry(countryCode, context);
+      });
+    });
+  }
+
+  Future<void> getData(BuildContext context, User data) async {
+    if (kDebugMode) {
+      print('getData called');
+    }
+    try {
+      userModel = await Provider.of<AuthController>(context, listen: false)
+          .getUserData(data.uid)
+          .first;
+      final userProvider = Provider.of<UserProvider>(context, listen: false);
+      userProvider.updateUser(userModel);
+      setState(() {});
+      if (kDebugMode) {
+        print('getData completed');
+      }
+    } catch (error) {
+      if (kDebugMode) {
+        print('Error in getData: $error');
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+
+    return Consumer<User?>(
+      builder: (context, data, child) {
+        return MaterialApp.router(
+          theme: ThemeData(
+            textTheme: TextTheme(
+              displayLarge: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Poppins',
+                fontSize: height * 0.02,
+                color: Colors.black,
               ),
-              colorScheme: const ColorScheme(
-                brightness: Brightness.light,
-                primary: Color(0xFF0C54BE),
-                onPrimary: Colors.white,
-                secondary: Color(0xFF303F60),
-                onSecondary: Colors.white,
-                error: Colors.red,
-                onError: Colors.white,
-                surface: Color(0xFFF5F9FD),
-                onSurface: Color(0xFFCED3DC),
+              displayMedium: TextStyle(
+                fontWeight: FontWeight.normal,
+                fontFamily: 'Poppins',
+                fontSize: height * 0.015,
+                color: Colors.black,
+              ),
+              bodyLarge: TextStyle(
+                fontFamily: 'Poppins',
+                fontSize: height * 0.01,
+                color: Colors.black,
               ),
             ),
-            debugShowCheckedModeBanner: false,
-            routerDelegate: RoutemasterDelegate(
-              routesBuilder: (context) {
-                if (data != null) {
+            colorScheme: const ColorScheme(
+              brightness: Brightness.light,
+              primary: Color(0xFF0C54BE),
+              onPrimary: Colors.white,
+              secondary: Color(0xFF303F60),
+              onSecondary: Colors.white,
+              error: Colors.red,
+              onError: Colors.white,
+              surface: Color(0xFFF5F9FD),
+              onSurface: Color(0xFFCED3DC),
+            ),
+          ),
+          debugShowCheckedModeBanner: false,
+          routerDelegate: RoutemasterDelegate(
+            routesBuilder: (context) {
+              if (data != null) {
+                if (kDebugMode) {
                   print('User is logged in');
-                  return loggedInRoute;
                 }
+                return loggedInRoute;
+              }
+              if (kDebugMode) {
                 print('User is not logged in');
-                return loggedOutRoute;
-              },
-            ),
-            routeInformationParser: const RoutemasterParser(),
-          );
-        },
-      ),
+              }
+              return loggedOutRoute;
+            },
+          ),
+          routeInformationParser: const RoutemasterParser(),
+        );
+      },
     );
   }
 }
